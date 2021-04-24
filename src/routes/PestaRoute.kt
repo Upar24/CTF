2c49485a02658612a44b4de7606d99e8ee8af0ca
Upar24 <alfionita.fina24@gmail.com>
2021-04-24 15:10:03 +0700

    post

--- src/routes/PestaRoute.kt
+++ src/routes/PestaRoute.kt
@@ -39,7 +39,7 @@ fun Route.pestaRoute(){
             }
         }
     }
-    route("/pot"){
+    route("/Pot"){
             get{
                 val pot = try {
                     call.receive<PotRequest>()
